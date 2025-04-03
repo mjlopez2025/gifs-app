43940a515ce9ab14093bd074c989bb2ff71a4bff
mjlopez <maximilianojlopez@hotmail.com>
2025-04-03 16:12:14 -0300

    6to commit

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,17 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { GifMapper } from '../mapper/gif.mapper';
+import { Gif } from '../interfaces/gif.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
   private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([])
+
 
   constructor() {
     this.loadTrendingGifs();
@@ -19,6 +24,11 @@ export class GifService {
         api_key: environment.giphyApiKey,
         limit: 20,
       },
+    })
+    .subscribe((resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGifs.set(gifs);
+      console.log({gifs});
     });
   }
 }
